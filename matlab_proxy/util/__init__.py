642af1b0fcd722cbef2befd4887fa89ee1c0c074
Eshan Patel <epatel@mathworks.com>
2023-09-26 17:16:27 +0530

    changing how the access_url for the server is formed

--- matlab_proxy/util/__init__.py
+++ matlab_proxy/util/__init__.py
@@ -232,14 +232,9 @@ def get_access_url(app):
 
     # NOTE: When windows container support is introduced this will need to be tweaked accordingly.
     if host_interface == "0.0.0.0" and system.is_windows():
-        import socket
+        host_interface = '127.0.0.1'
 
-        hostname = socket.gethostname()
-        fqdn = socket.getfqdn(hostname)
-
-        url = f"{access_protocol}://{fqdn}:{port}{base_url}"
-    else:
-        url = f"{access_protocol}://{host_interface}:{port}{base_url}"
+    url = f"{access_protocol}://{host_interface}:{port}{base_url}"
 
     return url
 
--- matlab_proxy/util/mwi/embedded_connector/request.py
+++ matlab_proxy/util/mwi/embedded_connector/request.py
@@ -105,6 +105,7 @@ async def get_state(mwi_server_url, headers=None):
         if not resp["messages"]["PingResponse"][0]["messageFaults"]:
             return "up"
     except Exception:
+        logger.debug(f"{err}: Embbeded connector is currently not responding to ping requests.")
         pass
 
     return "down"
